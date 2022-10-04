13ba9cdba398c890a83636bbad50a078341caab9
Marco Antonio Gutierrez <52805694+KyM1809@users.noreply.github.com>
2022-10-03 22:40:54 -0500

    Inicio

---
+++ B64CD/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace B64CD
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.label1 = new System.Windows.Forms.Label();
+			this.BtnAbrirArchivo = new System.Windows.Forms.Button();
+			this.TbOriginal = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.BtnCodificar = new System.Windows.Forms.Button();
+			this.BtnDecodificar = new System.Windows.Forms.Button();
+			this.TbCodificado = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(10, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(61, 20);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "Texto 1";
+			// 
+			// BtnAbrirArchivo
+			// 
+			this.BtnAbrirArchivo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.BtnAbrirArchivo.Location = new System.Drawing.Point(1062, 9);
+			this.BtnAbrirArchivo.Name = "BtnAbrirArchivo";
+			this.BtnAbrirArchivo.Size = new System.Drawing.Size(127, 28);
+			this.BtnAbrirArchivo.TabIndex = 1;
+			this.BtnAbrirArchivo.Text = "Abrir archivo";
+			this.BtnAbrirArchivo.UseVisualStyleBackColor = true;
+			this.BtnAbrirArchivo.Click += new System.EventHandler(this.BtnAbrirArchivo_Click);
+			// 
+			// TbOriginal
+			// 
+			this.TbOriginal.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TbOriginal.Location = new System.Drawing.Point(14, 43);
+			this.TbOriginal.Multiline = true;
+			this.TbOriginal.Name = "TbOriginal";
+			this.TbOriginal.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.TbOriginal.Size = new System.Drawing.Size(1175, 236);
+			this.TbOriginal.TabIndex = 2;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(10, 329);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(61, 20);
+			this.label2.TabIndex = 3;
+			this.label2.Text = "Texto 2";
+			// 
+			// BtnCodificar
+			// 
+			this.BtnCodificar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.BtnCodificar.Location = new System.Drawing.Point(969, 285);
+			this.BtnCodificar.Name = "BtnCodificar";
+			this.BtnCodificar.Size = new System.Drawing.Size(94, 33);
+			this.BtnCodificar.TabIndex = 4;
+			this.BtnCodificar.Text = "Codificar";
+			this.BtnCodificar.UseVisualStyleBackColor = true;
+			this.BtnCodificar.Click += new System.EventHandler(this.BtnCodificar_Click);
+			// 
+			// BtnDecodificar
+			// 
+			this.BtnDecodificar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.BtnDecodificar.Location = new System.Drawing.Point(1069, 285);
+			this.BtnDecodificar.Name = "BtnDecodificar";
+			this.BtnDecodificar.Size = new System.Drawing.Size(120, 33);
+			this.BtnDecodificar.TabIndex = 5;
+			this.BtnDecodificar.Text = "Decodificar";
+			this.BtnDecodificar.UseVisualStyleBackColor = true;
+			this.BtnDecodificar.Click += new System.EventHandler(this.BtnDecodificar_Click);
+			// 
+			// TbCodificado
+			// 
+			this.TbCodificado.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.TbCodificado.Location = new System.Drawing.Point(14, 352);
+			this.TbCodificado.Multiline = true;
+			this.TbCodificado.Name = "TbCodificado";
+			this.TbCodificado.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+			this.TbCodificado.Size = new System.Drawing.Size(1175, 236);
+			this.TbCodificado.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+			this.ClientSize = new System.Drawing.Size(1201, 616);
+			this.Controls.Add(this.TbCodificado);
+			this.Controls.Add(this.BtnDecodificar);
+			this.Controls.Add(this.BtnCodificar);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.TbOriginal);
+			this.Controls.Add(this.BtnAbrirArchivo);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "Form1";
+			this.Text = "B64 Codificar/Decodificar";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Button BtnAbrirArchivo;
+		private System.Windows.Forms.TextBox TbOriginal;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button BtnCodificar;
+		private System.Windows.Forms.Button BtnDecodificar;
+		private System.Windows.Forms.TextBox TbCodificado;
+	}
+}
+
---
+++ B64CD/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace B64CD
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		private void BtnAbrirArchivo_Click(object sender, EventArgs e)
+		{
+			var fileContent = string.Empty;
+			var filePath = string.Empty;
+			String file64 = "";
+			String extension = "";
+			Byte[] bytes = null;
+
+			using (OpenFileDialog openFileDialog = new OpenFileDialog())
+			{
+				//openFileDialog.InitialDirectory = "c:\\";
+				openFileDialog.Filter = "All files (*.*)|*.*|txt files (*.txt)|*.txt|jpg files (*.jpg)|*.jpg|png files (*.png)|*.png|jpeg files (*.jpeg)|*.jpeg|gif files (*.gif)|*.gif|webp files (*.webp)|*.webp|svg files (*.svg)|*.svg|bmp files (*.bmp)|*.bmp";
+				//openFileDialog.FilterIndex = 2;
+				openFileDialog.RestoreDirectory = false;
+
+				if (openFileDialog.ShowDialog() == DialogResult.OK)
+				{
+					//Get the path of specified file
+					filePath = openFileDialog.FileName;
+
+					//Read the contents of the file into a stream
+					//var fileStream = openFileDialog.OpenFile();
+
+					try
+					{
+						bytes = File.ReadAllBytes(filePath);
+						file64 = Convert.ToBase64String(bytes);
+					}
+					catch(Exception Ex1) { }
+					extension = Path.GetExtension(filePath).ToLower();
+					extension = extension.Replace(".","");
+					TbOriginal.Text = Encoding.UTF8.GetString(bytes);
+
+					if (extension == "jpg" || extension == "png" || extension == "jpeg" || extension == "gif" || extension == "webp" || extension == "svg" || extension == "bmp")
+					{
+						if(extension == "jpg")
+						{
+							TbCodificado.Text = "data:image/jpg;base64," + file64;
+						}
+						if(extension == "png")
+						{
+							TbCodificado.Text = "data:image/png;base64," + file64;
+						}
+						if(extension == "jpeg")
+						{
+							TbCodificado.Text = "data:image/jpeg;base64," + file64;
+						}
+						if(extension == "bmp")
+						{
+							TbCodificado.Text = "data:image/bmp;base64," + file64;
+						}
+						if (extension == "gif")
+						{
+							TbCodificado.Text = "data:image/png;base64," + file64;
+						}
+						if (extension == "svg")
+						{
+							TbCodificado.Text = "data:image/svg+xml;base64," + file64;
+						}
+						if (extension == "webp")
+						{
+							TbCodificado.Text = "data:image/webp;base64," + file64;
+						}
+					}
+					else
+					{
+						TbCodificado.Text = file64;
+					}
+				}
+			}
+		}
+
+		private void BtnCodificar_Click(object sender, EventArgs e)
+		{
+			byte[] texto_bytes = Encoding.UTF8.GetBytes(TbOriginal.Text);
+			TbCodificado.Text = Convert.ToBase64String(texto_bytes);
+		}
+
+		private void BtnDecodificar_Click(object sender, EventArgs e)
+		{
+			byte[] texto_byte = Convert.FromBase64String(TbOriginal.Text);
+			TbCodificado.Text = Encoding.UTF8.GetString(texto_byte);
+		}
+	}
+}
